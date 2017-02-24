0581b4b7f4fe83c3a624deaacf758b9c758f735e
Dan Beyer <dan.beyer@gmail.com>
2017-02-23 16:34:16 -0800

    Add files via upload

---
+++ Block3/smallsh.c
@@ -0,0 +1,12 @@
+/********************************************************************
+ * Name: Daniel Beyer
+ * CS344 - Program 3
+ * Description: This is a small shell written in C that will run command line instructions
+ * and return results similar to other shells.  The shell supports three built in commands:
+ * "exit", "cd" and "status".  It also supports comments as lines beginning with "#".
+ * *********************************************************************/
+
+#include <sys/types.h>
+#include <unist.h>
+#include <stdio.h>
+#include <stdlib.h>
